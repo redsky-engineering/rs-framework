06c5a9a02caafcb1d9af1a11e8bc0c5962e0648b
Joshua Hintze <joshua.hintze@gmail.com>
2022-04-08 18:30:18 -0600

    - grrrr

--- rollup.config.js
+++ rollup.config.js
@@ -1,56 +1,39 @@
 /**** NOTE
  * To understand how we are using this file see this url: https://www.codefeetime.com/post/rollup-config-for-react-component-library-with-typescript-scss/
  */
-
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import visualizer from 'rollup-plugin-visualizer';
-// import dts from 'rollup-plugin-dts';
+import peerDeps from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import visualizer from 'rollup-plugin-visualizer';
 import { terser } from 'rollup-plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require('./package.json');
-
-export default [
-	{
-		input: 'src/index.ts',
-		output: [
-			// {
-			// 	file: packageJson.main,
-			// 	format: 'cjs',
-			// 	sourcemap: true,
-			// 	// preserveModules: true,
-			// 	// dir: 'dist'
-			// },
-			{
-				// file: packageJson.module,
-				format: 'esm',
-				sourcemap: true,
-				preserveModules: true,
-				preserveModulesRoot: 'src',
-				dir: 'dist'
-			}
-		],
-		plugins: [
-			peerDepsExternal(),
-			resolve(),
-			commonjs(),
-			typescript({ tsconfig: './tsconfig.json' }),
-			postcss(),
-			terser(),
-			visualizer({
-				filename: 'bundle-analysis.html',
-				open: true
-			})
-		]
-	}
-	// {
-	// 	input: 'dist/esm/types/index.d.ts',
-	// 	output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-	// 	plugins: [dts()],
-	//
-	// 	external: [/\.(css|less|scss)$/]
-	// }
-];
+export default {
+	input: './src/index.ts',
+	output: {
+		dir: 'dist',
+		format: 'esm',
+		preserveModules: true,
+		preserveModulesRoot: 'src',
+		sourcemap: 'inline'
+	},
+	plugins: [
+		peerDeps(),
+		resolve(),
+		commonjs(),
+		typescript({
+			tsconfig: './tsconfig.json',
+			declaration: true,
+			declarationDir: 'dist'
+		}),
+		postcss({
+			sourceMap: 'inline'
+		}),
+		terser(),
+		visualizer({
+			filename: 'bundle-analysis.html',
+			open: true
+		})
+	]
+};
--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 // import './Button.scss';
 // import { Box } from '@bit/redsky.framework.rs.996';
+import classNames from 'classnames';
 
 export interface ButtonProps {}
 
 const Button: React.FC<ButtonProps> = (props) => {
-	return <button className={'rsButton'}>{props.children}</button>;
+	return <button className={classNames('rsButton')}>{props.children}</button>;
 };
 
-console.log('test');
 export default Button;
