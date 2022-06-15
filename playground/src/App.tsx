7ab769e98bc1f02ea59703d09d8c7bf44578685e
Suess <alex@redskytech.io>
2022-06-15 16:20:53 -0600

    feat(Accordion): Imported and updated the Accordion component to match new framework rules. feat(Select): Imported and updated the Select component to match new framework

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -29,28 +29,6 @@ function App() {
 	return (
 		<div className="App">
 			<View key="main" id="main" default initialPath="/" />
-			{/*<header className="App-header">*/}
-			{/*	<img src={logo} className="App-logo" alt="logo" />*/}
-			{/*	<p>Hello Vite + React!</p>*/}
-			{/*	<Label*/}
-			{/*		variant={'h3'}*/}
-			{/*		onClick={async () => {*/}
-			{/*			if (!(await formGroup.isValid())) {*/}
-			{/*				setFormGroup(formGroup.clone());*/}
-			{/*				console.log('invalid');*/}
-			{/*				return;*/}
-			{/*			}*/}
-			{/*			let test = formGroup.toModel();*/}
-			{/*			console.log(test);*/}
-			{/*		}}*/}
-			{/*	>*/}
-			{/*		H3 Label*/}
-			{/*	</Label>*/}
-			{/*	<InputText*/}
-			{/*		type={'text'}*/}
-			{/*		control={formGroup.get('test')}*/}
-			{/*		updateControl={(control) => setFormGroup(formGroup.clone().update(control))}*/}
-			{/*	/>*/}
 			<Chip labelVariant={'subtitle1'} label={'Hello World'} look={'standard'} chipStyles={{ color: 'black' }} />
 			<Button mt={20} look={'containedPrimary'}>
 				<Icon iconImg={'icon-close'} />I am a button
