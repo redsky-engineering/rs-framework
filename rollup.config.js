a1dc63abf4272eb2141c4c28a281a0d64d3f0a7e
Joshua Hintze <joshua.hintze@gmail.com>
2022-06-22 23:58:51 -0600

    feat(Select): working select with multi and create

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,7 @@ export default {
 		'react-select',
 		'react-toastify',
 		'axios',
-		'react-select'
+		'react-select',
+		'react-select/creatable/dist/react-select.esm.js'
 	]
 };
--- src/ui/select/Select.tsx
+++ src/ui/select/Select.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import './Select.scss';
 import { Box } from '../box/Box';
 import classNames from 'classnames';
@@ -16,8 +17,11 @@ import {
 import { ICommon } from '../../common/Interfaces';
 import { renderErrors } from '../../utils/internal';
 import clone from 'lodash.clone';
-import { useEffect, useState } from 'react';
-import CreatableSelect from 'react-select/creatable';
+
+// For webpack to work I had to import CreatableSelect with full qualified path, which means we lose on the types. Ts-ignore for now
+// @ts-ignore
+import CreatableSelect from 'react-select/creatable/dist/react-select.esm.js';
+import { PropsValue } from 'react-select/dist/declarations/src/types';
 
 export interface SelectProps<
 	Option,
@@ -56,6 +60,7 @@ function Select<Option, IsMulti extends boolean = false, Group extends GroupBase
 		marginY,
 		onChange,
 		isCreatable,
+		createOptionPosition,
 		onCreateOption,
 		...selectProps
 	} = props;
@@ -77,19 +82,29 @@ function Select<Option, IsMulti extends boolean = false, Group extends GroupBase
 		...(marginY && { marginY })
 	};
 
-	const [value, setValue] = useState<SingleValue<Option>>();
+	const [value, setValue] = useState<PropsValue<Option>>();
 
 	useEffect(() => {
 		if (!control || !props.options) return;
 
+		// Todo: We don't handle groups at this time, if we were to we would recurse through all groups selecting the options
 		if (isGroupOption(props.options)) return;
 
-		let optionFound = props.options.find((item) => {
-			// @ts-ignore
-			return item.value === control.value;
-		}) as Option;
-		if (!optionFound) setValue(undefined);
-		setValue(optionFound);
+		if (Array.isArray(control.value)) {
+			let optionsFound = props.options.filter((item) => {
+				// @ts-ignore
+				return control.value.includes(item.value);
+			}) as Option[];
+			if (!optionsFound.length) setValue(undefined);
+			else setValue(optionsFound);
+		} else {
+			let optionFound = props.options.find((item) => {
+				// @ts-ignore
+				return item.value === control.value;
+			}) as Option;
+			if (!optionFound) setValue(undefined);
+			else setValue(optionFound);
+		}
 	}, [control]);
 
 	function isMultiValue(value: MultiValue<any> | SingleValue<any>): value is MultiValue<any> {
@@ -97,10 +112,30 @@ function Select<Option, IsMulti extends boolean = false, Group extends GroupBase
 	}
 
 	function isGroupOption(options: OptionsOrGroups<Option, Group>): options is Group[] {
-		return !!props.isMulti;
+		// A group has a format like this:
+		// [
+		// 	{
+		// 		label: 'Colours',
+		// 		options: colourOptions,
+		// 	},
+		// 	{
+		// 		label: 'Flavours',
+		// 		options: flavourOptions,
+		// 	},
+		// ];
+		// Where as an option has this format
+		// [
+		// 	{
+		// 		label: 'Option 1',
+		// 		value: 1,
+		// 	}
+		// ]
+
+		// The best we can do to tell them apart is if there is an options value on one of the element
+		return !!options && Array.isArray(options) && options.length > 1 && options[0].hasOwnProperty('options');
 	}
 
-	function handleChange(value: OnChangeValue<Option, IsMulti>, action: ActionMeta<Option>) {
+	async function handleChange(value: OnChangeValue<Option, IsMulti>, action: ActionMeta<Option>) {
 		if (!control || !updateControl) return;
 		const updated = clone(control);
 
@@ -108,185 +143,52 @@ function Select<Option, IsMulti extends boolean = false, Group extends GroupBase
 			if (typeof updated.value === 'number') updated.value = Date.now();
 			else if (typeof updated.value === 'string') updated.value = '';
 			else if (Array.isArray(updated.value)) updated.value = [];
+			updated.clearErrors();
 			updateControl(updated);
 			return;
 		}
 
-		// TODO: Currently we don't support multi select options
-		if (isMultiValue(value)) return;
-		updated.value = (value as SingleValue<any>).value;
+		if (isMultiValue(value)) {
+			updated.value = value.map((item) => {
+				return item.value;
+			});
+		} else {
+			updated.value = (value as SingleValue<any>).value;
+		}
+		await updated.validate();
 		updateControl(updated);
+	}
+
+	function renderSelectOrCreateSelect() {
+		return !isCreatable ? (
+			<ReactSelect
+				{...selectProps}
+				onChange={(value, action) => {
+					if (onChange) onChange(value, action);
+					handleChange(value, action);
+				}}
+				value={value}
+			/>
+		) : (
+			<CreatableSelect
+				{...selectProps}
+				createOptionPosition={createOptionPosition}
+				onCreateOption={onCreateOption}
+				onChange={(value: OnChangeValue<Option, IsMulti>, action: ActionMeta<Option>) => {
+					if (onChange) onChange(value, action);
+					handleChange(value, action);
+				}}
+				value={value}
+			/>
+		);
 	}
 
 	return (
 		<Box id={id} className={classNames('rsSelect', className)} {...boxMarginProps}>
-			{isCreatable ? (
-				<ReactSelect
-					{...selectProps}
-					onChange={(value, action) => {
-						if (onChange) onChange(value, action);
-						handleChange(value, action);
-						// if (action.action === 'clear') {
-						// 	if (props.onFormClear) props.onFormClear();
-						// }
-					}}
-					value={value}
-				/>
-			) : (
-				<CreatableSelect
-					{...selectProps}
-					onCreateOption={onCreateOption}
-					onChange={(value, action) => {
-						if (onChange) onChange(value, action);
-						handleChange(value, action);
-						// changeHandler(value);
-						// setValue(value);
-						// if (action.action === 'clear') {
-						// 	if (props.onFormClear) props.onFormClear();
-						// }
-					}}
-					value={value}
-				/>
-			)}
-
+			{renderSelectOrCreateSelect()}
 			{renderErrors(props.control)}
 		</Box>
 	);
 }
-
-// const Select: React.FC<SelectProps> = (props) => {
-// const [control, setControl] = useState(props.control);
-// const [value, setValue] = useState<OptionType[] | GroupType[]>();
-// useEffect(() => {
-// 	setControl(props.control);
-//
-// 	if (!Array.isArray(props.options) || !props.options.length) return;
-//
-// 	if (props.control.value.toString().length > 0) {
-// 		let controlValue = props.control.value;
-// 		let newOptionsArray = createOptionArray(props.options);
-// 		if (typeof controlValue === 'number' || typeof controlValue === 'string') {
-// 			setValue(
-// 				newOptionsArray.filter((item) => {
-// 					return item.value === props.control.value;
-// 				})
-// 			);
-// 		} else if (Array.isArray(controlValue)) {
-// 			let defaultOptions: OptionType[] = [];
-// 			controlValue.forEach((item: string | number) => {
-// 				let optionValue = newOptionsArray.filter((option) => {
-// 					return item === option.value;
-// 				});
-// 				defaultOptions = [...defaultOptions, ...optionValue];
-// 			});
-// 			setValue(defaultOptions);
-// 		}
-// 	}
-// }, [props.control, props.options]);
-//
-// function isGroupOption(options: OptionType[] | GroupType[]): options is GroupType[] {
-// 	return options[0].hasOwnProperty('options');
-// }
-//
-// function createOptionArray(options: OptionType[] | GroupType[]): OptionType[] {
-// 	let newOptionsArray: OptionType[] = [];
-// 	if (isGroupOption(options)) {
-// 		options.forEach((item) => {
-// 			newOptionsArray = [...newOptionsArray, ...item.options];
-// 		});
-// 	} else {
-// 		newOptionsArray = options;
-// 	}
-// 	return newOptionsArray;
-// }
-//
-// useEffect(() => {
-// 	if (control.errors.length > 0) {
-// 		let controlElement = document.querySelector("[class*='control']"); //control
-// 		let placeholderElement = document.querySelector(".rsSelect [class*='placeholder']"); //placeholder
-// 		if (!!controlElement && !!placeholderElement) {
-// 			controlElement.classList.add('error');
-// 			placeholderElement.classList.add('error');
-// 		}
-// 	} else {
-// 		let controlElement = document.querySelector("[class*='control']"); //control
-// 		let placeholderElement = document.querySelector(".rsSelect [class*='placeholder']"); //placeholder
-// 		if (!!controlElement && !!placeholderElement) {
-// 			controlElement.classList.remove('error');
-// 			placeholderElement.classList.remove('error');
-// 		}
-// 	}
-// }, [control.errors]);
-//
-// async function changeHandler(event: OptionType | GroupType) {
-// 	let target: string | number | string[] | number[] = '';
-// 	if (Array.isArray(event)) {
-// 		target = event.map((item) => {
-// 			return item.value;
-// 		});
-// 		if (target.length === 0) target = props.isMulti ? [] : '';
-// 	} else {
-// 		if (event === null) target = props.isMulti ? [] : '';
-// 		else target = (event as OptionType).value;
-// 	}
-//
-// 	const updated = clone(props.control);
-// 	if (updated) {
-// 		updated.value = target;
-// 		if (typeof updated.value !== 'number') {
-// 			if (updated.value.length === 0) {
-// 				updated.clearErrors();
-// 			} else {
-// 				await updated.validate();
-// 			}
-// 		}
-//
-// 		setControl(updated);
-// 		if (props.updateControl) props.updateControl(updated);
-// 	}
-// 	if (props.onRsFormChange) props.onRsFormChange(event, updated);
-// }
-// function renderErrors() {
-// 	const errorNodes: React.ReactNode[] = [];
-// 	const errors = control.errors;
-// 	for (let index = 0; index < errors.length; index++) {
-// 		const errorMessage = control.getErrorMessage(errors[index]);
-// 		errorNodes.push(
-// 			<div key={`${index}Error`} className="rsInputErrorMessage">
-// 				{errorMessage}
-// 			</div>
-// 		);
-// 	}
-// 	return errorNodes;
-// }
-//
-// function renderStyles() {
-// 	if (!props.width) return;
-// 	let width = props.width;
-// 	if (typeof props.width === 'number') width += 'px';
-//
-// 	return { width: width };
-// }
-//
-// let SelectComponent: any = props.isCreatable ? Creatable : ReactSelect;
-//
-// return (
-// 	<Box className={classNames('rsSelect', props.className)} style={renderStyles()}>
-// 		{
-// 			<SelectComponent
-// 				{...props}
-// 				onChange={(value: any, action: any) => {
-// 					changeHandler(value);
-// 					setValue(value);
-// 					if (action.action === 'clear') {
-// 						if (props.onFormClear) props.onFormClear();
-// 					}
-// 				}}
-// 				value={value}
-// 			/>
-// 		}
-// 		{renderErrors()}
-// 	</Box>
-// );
 
 export { Select };
