a5ded1a7033d48dba4d31d9232cf674e7795afd0
Suess <alex@redskytech.io>
2022-09-08 15:14:19 -0600

    feat(Utils): Added AppUtils class to utils file.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-export { StringUtils, NumberUtils, DateUtils, ObjectUtils, RegionUtils, WebUtils } from './Utils';
+export { StringUtils, NumberUtils, DateUtils, ObjectUtils, RegionUtils, WebUtils, AppUtils } from './Utils';
 export { HttpClient } from './HttpClient';
