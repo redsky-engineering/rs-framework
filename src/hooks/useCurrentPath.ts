62eddec35835a46056bc0e6bdd3e10c508f9da88
Joshua Hintze <joshua.hintze@gmail.com>
2023-01-03 14:28:16 -0700

    fix(hook): Fixed initial use of useCurrentPath()

--- src/hooks/useCurrentPath.ts
+++ src/hooks/useCurrentPath.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Router } from '../996';
 
 export function useCurrentPath() {
-	const [currentPath, setCurrentPath] = useState<string>('');
+	const [currentPath, setCurrentPath] = useState<string>(window.location.pathname);
 	useEffect(() => {
 		let id = Router.getInstance().subscribeToAfterRouterNavigate((newPath) => {
 			setCurrentPath(newPath);
